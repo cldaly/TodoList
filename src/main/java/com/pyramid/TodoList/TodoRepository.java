aa4c3208e95d974104244907f6da31580b338e77
Guram <gr95@protonmail.com>
2020-01-14 11:43:05 -0500

    yes

--- src/main/java/com/pyramid/TodoList/TodoRepository.java
+++ src/main/java/com/pyramid/TodoList/TodoRepository.java
@@ -2,6 +2,6 @@ package com.pyramid.TodoList;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TodoRepository extends MongoRepository<Todo, Integer> {
+public interface TodoRepository extends MongoRepository<Todo, String> {
 
 }
