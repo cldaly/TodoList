a60c47c3f4fce10c4490a4219c44c23b0f3c5db8
Guram <gr95@protonmail.com>
2020-01-14 13:11:45 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/com/pyramid/TodoList/TodoService.java
+++ src/main/java/com/pyramid/TodoList/TodoService.java
@@ -24,6 +24,7 @@ public class TodoService {
         repo.save(todo);
     }
     public Optional<List<Todo>> findAll(){
-        return repo.findByisCompleteFalse();
+        return repo.findByIsCompleteFalse();
     }
+
 }
